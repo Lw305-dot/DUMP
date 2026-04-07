dab8844567255896fcbdebb4646c2c6dd2f80548
Lw305-dot <lenwillsystem@gmail.com>
2026-04-07 10:44:35 +0000

    adding evaluation for

--- survey.py
+++ survey.py
@@ -2,11 +2,11 @@ import qrcode
 from pathlib import Path
 output_dir = Path("/workspaces/DUMP")
 # link="https://docs.google.com/forms/d/e/1FAIpQLSc388co6eV7ZoCMTeVieP1WsfcPzUlXgZTSw3sJUExN4mQvTw/viewform?usp=dialog"
-link="https://docs.google.com/forms/d/e/1FAIpQLSf14yetsF-HkQmVRPFRSME_yYjt2XziofRsiy_s2oKbvgqJXA/viewform?usp=sharing&ouid=112209182449128421490"
+link="https://docs.google.com/forms/d/e/1FAIpQLSdaz7M65ST4NotTmaQ5gkLrwSW4YEdzuQjrpfJ3Kp99hbDV6w/viewform"
 Qr=qrcode.QRCode(version=1,box_size=10,border=4)
 Qr.add_data(link)
 Qr.make(fit=True)
 img = Qr.make_image(fill_color="black", back_color="white")
-output_path = output_dir / "Health_and_safety_qr2.png"
+output_path = output_dir / "Evaluationform.png"
 img.save(output_path)
 print(f"QR code saved to: {output_path}")
