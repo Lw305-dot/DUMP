54c02463693bc210eeddd7d996630425800e9c85
Lw305-dot <lenwillsystem@gmail.com>
2026-08-06 07:01:00 +0000

    adding report 51

--- Working Files/Final July12 ver1.py
+++ Working Files/Final July12 ver1.py
@@ -15,7 +15,7 @@ training_list_path = Path("/workspaces/DUMP/MASTER LIST Module number.xlsx")
 matartraining_list_path = Path("/workspaces/DUMP/Matar Trainings.xlsx")
 training_sheets = pd.read_excel(training_list_path, sheet_name=["Manuals", "SOPs"])
 training_lookup_df = pd.concat(training_sheets.values(), ignore_index=True)
-output_dir = Path("/workspaces/DUMP/Employee_Reports_wi50")
+output_dir = Path("/workspaces/DUMP/Employee_Reports_wi51")
 output_dir.mkdir(exist_ok=True)   
 
 
